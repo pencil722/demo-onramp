9b30b4d627ac5f215cc5018223bfa321260c1448
pencil722 <pencil722@gmail.com>
2021-10-18 16:39:46 +0800

    adjust js to get campaign id from page

--- record.js
+++ record.js
@@ -7,9 +7,12 @@
     };
 
     function recordAndGet() {
+        var urlParams = new URLSearchParams(window.location.search);
+        var campaign_id = urlParams.get('id')
+        
         $.ajax({
             type: 'POST',
-            url: '/api/campaign/record_data?campaign=AW-300000000',
+            url: '/api/campaign/record_data?campaign=' + campaign_id,
             data: JSON.stringify(dataJSON),
             contentType: 'application/json; charset=utf-8',
             dataType: 'json',
@@ -18,6 +21,5 @@
             }
         })
     };
-
 
 </script>
